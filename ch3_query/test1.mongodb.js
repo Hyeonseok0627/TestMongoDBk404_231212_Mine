1fe8ae04c2f4eeeaaade8efc5dcde3469a12a7d0
Hyeonseok0627 <stonehead0627@gmail.com>
2023-12-13 11:42:50 +0900

    231213(2교시 몽고디비 공공 데이터 차량사고 임포트 간단 조회, 수정 샘플코드 확인 / 3교시 쿼리 조건 확인 테스트)

--- ch3_query/test1.mongodb.js
+++ ch3_query/test1.mongodb.js
@@ -20,6 +20,8 @@
 // find(<query>,<projection>)
 // query: 조건부, projection: 보고싶은 열
 
+// 3장의 5페이지 실습 내용)
+
 // "교차로내.accident_count" 이것만 보여주고, 나머지 열 안보여줘도 됨. -> 단, _id는 제외.
 db.by_road_type.find({county:"강릉시"},{"교차로내.accident_count":1});
 
@@ -28,3 +30,86 @@ db.by_road_type.find({county:"강릉시"},{"교차로내.accident_count":1, _id:
 
 // 다보여줘. select *
 db.by_road_type.find({ county: "강릉시" });
+
+db.by_road_type.find({ county: "강릉시" },{"교차로내.accident_count" : 1, "터널안.accident_count" : 1});
+
+db.by_road_type.find({'기타단일로.death_toll':0}, {city_or_province:1, county:1});
+
+// 3장 7페이지 실습내용)
+// use("test");
+db.inventory.insertMany([
+  { item: "journal", qty: 25, tags: ["blank", "red"] },
+  { item: "notebook", qty: 50, tags: ["red", "blank"] },
+  { item: "paper", qty: 100, tags: ["yellow", "pink"] },
+  { item: "planner", qty: 75, tags: ["blank", "red"] },
+  { item: "postcard", qty: 45, tags: ["blue"] }
+  ]);
+
+  // 쿼리 예제, 한문장씩 확인해보기. (projection이 생략됨 -> 모든 컬럼 보여줘.)
+db.inventory.find({item:{$eq:'journal'}});
+
+db.inventory.find({item:'journal'});
+
+db.inventory.find({tags: {$in : ['red']}});
+
+db.inventory.find({tags: {$nin:["blank","blue"]}});
+
+db.inventory.find({tags: {$in : [/^[a-z]*d/]}});
+
+db.inventory.find({tags: {$in : [/^b/]}});
+
+db.inventory.find({qty:{$not:{$gt:50}}});
+
+db.inventory.find({qty:{$lte:50}});
+
+db.inventory.find({$or:[{qty:{$gt:90}},{qty:{$lt:50}}]}); //논리연산자(or)가 조건보다 앞에 있는게 앞선 것들과는 다른점
+
+db.inventory.find({$and:[{qty:{$gt:50}},{qty:{$lt:90}}]});
+
+db.inventory.find({qty:{$gt:50,$lt:90}});
+
+// 검색
+db.stores.insert(
+  [
+  { _id: 1, name: "Java Hut", description: "Coffee and cakes" },
+  { _id: 2, name: "Burger Buns", description: "Gourmet hamburgers" },
+  { _id: 3, name: "Coffee Shop", description: "Just coffee" },
+  { _id: 4, name: "Clothes Clothes Clothes", description: "Discount clothing" },
+  { _id: 5, name: "Java Shopping", description: "Indonesian goods" }
+  ]
+  );
+
+// #Text index
+db.stores.createIndex({name:"text",description:"text"});
+
+// $text Operation
+db.stores.find( { $text: { $search: "java coffee shop" } } );
+
+db.stores.find( { $text: { $search: "java shop" } } );
+
+db.stores.find( { $text: { $search: "shop" } } );
+
+//  Exact Phrase : 정확하게 일치하는 문서 찾기
+db.stores.find( { $text: { $search: "\"coffee shop\"" } } );
+
+db.stores.find( { $text: { $search: "Coffee and" } } );
+
+//  Term Exclustion : "-"연산자를 사용하여 검색에 제외할 텀을 지정
+db.stores.find( { $text: { $search: "java shop -coffee" } } );
+
+db.stores.find( { $text: { $search: "-java shop coffee" } } );
+
+// tags가 "red", "blank" 둘 다 주어진 순서대로 가진 도큐먼트 전부 출력
+db.inventory.find({tags: ["red", "blank"]});
+
+db.collection.insertMany([
+  { item: "journal", qty: 25, tags: [10,20,30,40,50] },
+  { item: "notebook", qty: 50, tags: [1,2,3,4,5] },
+  { item: "paper", qty: 100, tags: [3,6,9,12,15] },
+  { item: "planner", qty: 75, tags: [7,8,9,10,11] },
+  { item: "postcard", qty: 45, tags: [2,4,6,8,10] }
+  ]);
+
+db.collection.find({tags: {$gt: 10, $lt: 15}});
+db.collection.find({tags: {$elemMatch: {$gt: 10, $lt: 15}}});
+
