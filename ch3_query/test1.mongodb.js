09339376ab88f1b485aa653e5950fcd7952671a2
Hyeonseok0627 <stonehead0627@gmail.com>
2023-12-13 11:52:14 +0900

    231213( 3교시 쿼리 조건 확인 테스트2)

--- ch3_query/test1.mongodb.js
+++ ch3_query/test1.mongodb.js
@@ -1,20 +1,17 @@
-
-// mongodb의 compass에서 
+// mongodb의 compass에서
 
 // database 새로 생성 -> test -> collection의 name은 샘플 json 이름 그대로 써서 생성 -> import -> 해당 json 파일 선택하면 등록이 됨.
 
 // 이렇게 4개 다 만들고나서 밑에 조회하면 나오게 됨.
 
 // 1 area 컬렉션 만들기
-// 1-2 area.json 임포트하기. 
+// 1-2 area.json 임포트하기.
 // 2 by_month 컬렉션 만들기
-// 2-2 by_month.json 임포트하기. 
+// 2-2 by_month.json 임포트하기.
 // 3 by_road_type 컬렉션 만들기
-// 3-2 by_road_type.json 임포트하기. 
+// 3-2 by_road_type.json 임포트하기.
 // 4 by_type 컬렉션 만들기
-// 4-2 by_type.json 임포트하기. 
-
-
+// 4-2 by_type.json 임포트하기.
 
 // use("test");
 // find(<query>,<projection>)
@@ -23,17 +20,26 @@
 // 3장의 5페이지 실습 내용)
 
 // "교차로내.accident_count" 이것만 보여주고, 나머지 열 안보여줘도 됨. -> 단, _id는 제외.
-db.by_road_type.find({county:"강릉시"},{"교차로내.accident_count":1});
+db.by_road_type.find({ county: "강릉시" }, { "교차로내.accident_count": 1 });
 
 // _id 제외해죠 select 교차로내.accident_count
-db.by_road_type.find({county:"강릉시"},{"교차로내.accident_count":1, _id: 0});
+db.by_road_type.find(
+  { county: "강릉시" },
+  { "교차로내.accident_count": 1, _id: 0 }
+);
 
 // 다보여줘. select *
 db.by_road_type.find({ county: "강릉시" });
 
-db.by_road_type.find({ county: "강릉시" },{"교차로내.accident_count" : 1, "터널안.accident_count" : 1});
+db.by_road_type.find(
+  { county: "강릉시" },
+  { "교차로내.accident_count": 1, "터널안.accident_count": 1 }
+);
 
-db.by_road_type.find({'기타단일로.death_toll':0}, {city_or_province:1, county:1});
+db.by_road_type.find(
+  { "기타단일로.death_toll": 0 },
+  { city_or_province: 1, county: 1 }
+);
 
 // 3장 7페이지 실습내용)
 // use("test");
@@ -42,74 +48,81 @@ db.inventory.insertMany([
   { item: "notebook", qty: 50, tags: ["red", "blank"] },
   { item: "paper", qty: 100, tags: ["yellow", "pink"] },
   { item: "planner", qty: 75, tags: ["blank", "red"] },
-  { item: "postcard", qty: 45, tags: ["blue"] }
-  ]);
+  { item: "postcard", qty: 45, tags: ["blue"] },
+]);
 
-  // 쿼리 예제, 한문장씩 확인해보기. (projection이 생략됨 -> 모든 컬럼 보여줘.)
-db.inventory.find({item:{$eq:'journal'}});
+// 쿼리 예제, 한문장씩 확인해보기. (projection이 생략됨 -> 모든 컬럼 보여줘.)
+db.inventory.find({ item: { $eq: "journal" } });
 
-db.inventory.find({item:'journal'});
+db.inventory.find({ item: "journal" });
 
-db.inventory.find({tags: {$in : ['red']}});
+db.inventory.find({ tags: { $in: ["red"] } });
 
-db.inventory.find({tags: {$nin:["blank","blue"]}});
+db.inventory.find({ tags: { $nin: ["blank", "blue"] } });
 
-db.inventory.find({tags: {$in : [/^[a-z]*d/]}});
+db.inventory.find({ tags: { $in: [/^[a-z]*d/] } });
 
-db.inventory.find({tags: {$in : [/^b/]}});
+db.inventory.find({ tags: { $in: [/^b/] } });
 
-db.inventory.find({qty:{$not:{$gt:50}}});
+db.inventory.find({ qty: { $not: { $gt: 50 } } });
 
-db.inventory.find({qty:{$lte:50}});
+db.inventory.find({ qty: { $lte: 50 } });
 
-db.inventory.find({$or:[{qty:{$gt:90}},{qty:{$lt:50}}]}); //논리연산자(or)가 조건보다 앞에 있는게 앞선 것들과는 다른점
+db.inventory.find({ $or: [{ qty: { $gt: 90 } }, { qty: { $lt: 50 } }] }); //논리연산자(or)가 조건보다 앞에 있는게 앞선 것들과는 다른점
 
-db.inventory.find({$and:[{qty:{$gt:50}},{qty:{$lt:90}}]});
+db.inventory.find({ $and: [{ qty: { $gt: 50 } }, { qty: { $lt: 90 } }] });
 
-db.inventory.find({qty:{$gt:50,$lt:90}});
+db.inventory.find({ qty: { $gt: 50, $lt: 90 } });
 
 // 검색
-db.stores.insert(
-  [
+db.stores.insert([
   { _id: 1, name: "Java Hut", description: "Coffee and cakes" },
   { _id: 2, name: "Burger Buns", description: "Gourmet hamburgers" },
   { _id: 3, name: "Coffee Shop", description: "Just coffee" },
   { _id: 4, name: "Clothes Clothes Clothes", description: "Discount clothing" },
-  { _id: 5, name: "Java Shopping", description: "Indonesian goods" }
-  ]
-  );
+  { _id: 5, name: "Java Shopping", description: "Indonesian goods" },
+]);
 
 // #Text index
-db.stores.createIndex({name:"text",description:"text"});
+db.stores.createIndex({ name: "text", description: "text" });
 
 // $text Operation
-db.stores.find( { $text: { $search: "java coffee shop" } } );
+db.stores.find({ $text: { $search: "java coffee shop" } });
 
-db.stores.find( { $text: { $search: "java shop" } } );
+db.stores.find({ $text: { $search: "java shop" } });
 
-db.stores.find( { $text: { $search: "shop" } } );
+db.stores.find({ $text: { $search: "shop" } });
 
 //  Exact Phrase : 정확하게 일치하는 문서 찾기
-db.stores.find( { $text: { $search: "\"coffee shop\"" } } );
+db.stores.find({ $text: { $search: '"coffee shop"' } });
 
-db.stores.find( { $text: { $search: "Coffee and" } } );
+db.stores.find({ $text: { $search: "Coffee and" } });
 
 //  Term Exclustion : "-"연산자를 사용하여 검색에 제외할 텀을 지정
-db.stores.find( { $text: { $search: "java shop -coffee" } } );
+db.stores.find({ $text: { $search: "java shop -coffee" } });
 
-db.stores.find( { $text: { $search: "-java shop coffee" } } );
+db.stores.find({ $text: { $search: "-java shop coffee" } });
 
 // tags가 "red", "blank" 둘 다 주어진 순서대로 가진 도큐먼트 전부 출력
-db.inventory.find({tags: ["red", "blank"]});
+db.inventory.find({ tags: ["red", "blank"] });
 
 db.collection.insertMany([
-  { item: "journal", qty: 25, tags: [10,20,30,40,50] },
-  { item: "notebook", qty: 50, tags: [1,2,3,4,5] },
-  { item: "paper", qty: 100, tags: [3,6,9,12,15] },
-  { item: "planner", qty: 75, tags: [7,8,9,10,11] },
-  { item: "postcard", qty: 45, tags: [2,4,6,8,10] }
-  ]);
+  { item: "journal", qty: 25, tags: [10, 20, 30, 40, 50] },
+  { item: "notebook", qty: 50, tags: [1, 2, 3, 4, 5] },
+  { item: "paper", qty: 100, tags: [3, 6, 9, 12, 15] },
+  { item: "planner", qty: 75, tags: [7, 8, 9, 10, 11] },
+  { item: "postcard", qty: 45, tags: [2, 4, 6, 8, 10] },
+]);
 
-db.collection.find({tags: {$gt: 10, $lt: 15}});
-db.collection.find({tags: {$elemMatch: {$gt: 10, $lt: 15}}});
+// all 조건(or개념) x > 10
+// all 조건 x < 15
+db.collection.find({ tags: { $gt: 10, $lt: 15 } });
+// elemMatch(and개념) : 10 < x < 15 : 11,12,13,14
+db.collection.find({ tags: { $elemMatch: { $gt: 10, $lt: 15 } } });
 
+db.by_month.find({
+  $and: [
+    { month_data: { $elemMatch: { month: "01월", heavy_injury: 0 } } },
+    { month_data: { $elemMatch: { month: "02월", death_toll: 0 } } },
+  ],
+});
